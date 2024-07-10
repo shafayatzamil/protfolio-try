95be30aa05967fb06e52c71b54f636481c319390
shafayat zamil <zamilkhan111.za@gmail.com>
2024-07-10 21:27:31 +0600

    some changes

--- src/app/page.js
+++ src/app/page.js
@@ -8,18 +8,18 @@ const HomePage = () => {
   return (
     <div>
       {/* header section  */}
-      <div className=" m-2  mt-6 border-2 rounded-lg p-2 h-96 ">
+      <div className=" m-2  mt-6 border-2 rounded-lg p-2  ">
         {/* short heading */}
         <div className="flex justify-between items-center gap-1 mt-3">
           <div>
-            <h4 className="text-gray-400 text-sm  font-bold flex items-center ">
+            <h4 className="text-gray-500 text-sm  font-bold flex items-center ">
               <span>
                 <Image src={grayIcon} height={16} width={16} alt="grayicons" />
               </span>
               Back-end Developer
             </h4>
           </div>
-          <div className="text-[#039754] bg-green-200 px-2 py-1 text-xs rounded-xl font-bold ">
+          <div className="text-[#039754] bg-green-100 px-2 py-1 text-xs rounded-xl font-bold ">
             <h4 className=" flex items-center justify-between">
               <span>
                 <Image src={blueIcon} height={12} width={12} alt="blueicons" />
@@ -29,10 +29,14 @@ const HomePage = () => {
           </div>
         </div>
 
-        <div className="flex justify-between items-center gap-2 mt-8 ">
-          <div>
-            <h2 className="font-bold text-3xl text-gray-600 my-4 ">
-              I’m Shafayat zamil
+        <div className=" grid grid-cols-5  gap-2 mt-8  ">
+          {/* heading name */}
+          <div className="p-2 col-span-3 ">
+            <h2 className="text-md font-bold text-gray-700 mb-3">
+              I’m
+              <span className="font-bold text-4xl text-gray-700">
+                Shafayat zamil
+              </span>
             </h2>
             <p className=" text-sm text-gray-500  ">
               Back End Developer based in Node js.Currently
@@ -41,7 +45,8 @@ const HomePage = () => {
               developing at express js.
             </span>
           </div>
-          <div>
+          {/* image section */}
+          <div className=" col-span-2 my-auto">
             {/* <Image
               src={ownerImage}
               height={96}
@@ -49,14 +54,12 @@ const HomePage = () => {
               className="rounded-3xl"
               alt="owner"
             /> */}
-            <div className="avatar online">
-              <div className="">
-                <Image
-                  alt="ownerimage"
-                  className="rounded-full w-48"
-                  src={ownerImage}
-                />
-              </div>
+            <div className="">
+              <Image
+                alt="ownerimage"
+                className="rounded-full w-48"
+                src={ownerImage}
+              />
             </div>
           </div>
         </div>
