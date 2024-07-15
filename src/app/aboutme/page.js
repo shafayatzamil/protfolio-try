3bc4194ebf5f96c9b948458384bef0ac49b85a6c
shafayat zamil <zamilkhan111.za@gmail.com>
2024-07-15 21:13:06 +0600

    abput page project added

--- src/app/aboutme/page.js
+++ src/app/aboutme/page.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 import React from "react";
 import grayIcon from "../../assets/grayIcon.png";
 import ownerImage from "../../assets/image (1).png";
+import projectLogo from "../../assets/image (1).png";
+import arrowIcon from "../../assets/icons8-arrow-24 (1).png";
 
 const page = () => {
   return (
@@ -28,11 +30,12 @@ const page = () => {
           is to bridge the divide between people and technology, transforming
           intricate challenges into meaningful and seamless experiences.
         </p>
-
+        {/* owner image  */}
         <div className="border-2 rounded-lg flex justify-center p-2 bg-gray-50">
           <Image src={ownerImage} alt="ownerimage" />
         </div>
 
+        {/* more info */}
         <div>
           <h2 className="text-3xl  mt-4 mb-2 text-gray-700 font-bold">
             More About Me
@@ -53,6 +56,79 @@ const page = () => {
             sure to take breaks and reenergize his creativity. In his spare
             time, he also volunteers at a local animal shelter on weekends.
           </p>
+        </div>
+
+        {/* side project */}
+        <div>
+          <h2 className="text-3xl mt-4 mb-2 text-gray-700 font-bold ">
+            My Recent Projects
+          </h2>
+
+          <p className="text-gray-600 text-md mb-4">
+            I did passion side projects in the weekend, please take a look you
+            will love it (i hope).
+          </p>
+
+          <div className="">
+            {/* this is project */}
+            <div className="flex justify-around items-center py-2 border-2 rounded-lg my-2 hover:shadow-md ">
+              <div className="border-2 p-2 rounded-full">
+                <Image
+                  src={projectLogo}
+                  weight={46}
+                  height={46}
+                  alt="projecticon"
+                ></Image>
+              </div>
+              <div className=" text-gray-500 -ml-4">
+                <h2 className="text-lg font-bold">Marba labs</h2>
+                <h2 className="text-sm font-semibold">
+                  visual designing,branding
+                </h2>
+              </div>
+              <div className="border-2 rounded-full">
+                <Image src={arrowIcon} alt="project arrow"></Image>
+              </div>
+            </div>
+            <div className="flex justify-around items-center py-2 border-2 rounded-lg my-2 hover:shadow-md ">
+              <div className="border-2 p-2 rounded-full">
+                <Image
+                  src={projectLogo}
+                  weight={46}
+                  height={46}
+                  alt="projecticon"
+                ></Image>
+              </div>
+              <div className=" text-gray-500 -ml-4">
+                <h2 className="text-lg font-bold">Marba labs</h2>
+                <h2 className="text-sm font-semibold">
+                  visual designing,branding
+                </h2>
+              </div>
+              <div className="border-2 rounded-full">
+                <Image src={arrowIcon} alt="project arrow"></Image>
+              </div>
+            </div>
+            <div className="flex justify-around items-center py-2  border-2 rounded-lg my-2 hover:shadow-md">
+              <div className="border-2 p-2 rounded-full">
+                <Image
+                  src={projectLogo}
+                  weight={46}
+                  height={46}
+                  alt="projecticon"
+                ></Image>
+              </div>
+              <div className=" text-gray-500 -ml-4">
+                <h2 className="text-lg font-bold">Marba labs</h2>
+                <h2 className="text-sm font-semibold">
+                  visual designing,branding
+                </h2>
+              </div>
+              <div className="border-2 rounded-full">
+                <Image src={arrowIcon} alt="project arrow"></Image>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
